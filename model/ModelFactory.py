2f47e636603c7e802aad9f96cdbde98ef7fed42a
kikumaru818 <kikumaru818@gmail.com>
2023-05-10 17:17:44 -0400

    Fixing bug with MX-sensei

--- model/ModelFactory.py
+++ model/ModelFactory.py
@@ -3,11 +3,11 @@ class ModelFactory():
 
     @classmethod
     def produce_model_and_tokenizer(cls, cfg, num_labels, id2label, label2id):
-        if cfg.lm == "GPTJ":
-            tokenizer = AutoTokenizer.from_pretrained("ydshieh/tiny-random-gptj-for-sequence-classification", num_labels=num_labels, id2label = id2label, label2id = label2id)
+        if "gptj" in cfg.lm:
+            model = GPTJForSequenceClassification.from_pretrained(cfg.lm, num_labels=num_labels, id2label = id2label, label2id = label2id, ignore_mismatched_sizes=True)
+            tokenizer = AutoTokenizer.from_pretrained(cfg.lm, model_max_length=model.config.max_position_embeddings)
             tokenizer.padding_side = "left"
             tokenizer.pad_token = tokenizer.eos_token
-            model = GPTJForSequenceClassification.from_pretrained("ydshieh/tiny-random-gptj-for-sequence-classification", num_labels=num_labels, id2label = id2label, label2id = label2id, ignore_mismatched_sizes=True)
         else:
             tokenizer = AutoTokenizer.from_pretrained(cfg.lm)
             model = AutoModelForSequenceClassification.from_pretrained(cfg.lm, num_labels=num_labels, id2label = id2label, label2id = label2id)
