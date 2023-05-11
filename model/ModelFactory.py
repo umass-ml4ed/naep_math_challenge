91fe0670f50304a9c4fd9c37185df55d7df7c00d
kikumaru818 <kikumaru818@gmail.com>
2023-05-10 21:28:46 -0400

    fix bugs in train.py

--- model/ModelFactory.py
+++ model/ModelFactory.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, GPTJForSequenceClassification
+from model.EncoderDecoder import FlanT5encoder
 class ModelFactory():
 
     @classmethod
@@ -8,6 +9,9 @@ class ModelFactory():
             tokenizer = AutoTokenizer.from_pretrained(cfg.lm, model_max_length=model.config.max_position_embeddings)
             tokenizer.padding_side = "left"
             tokenizer.pad_token = tokenizer.eos_token
+        elif 't5' in cfg.lm:
+            model = FlanT5encoder(cfg.lm, num_labels)
+            tokenizer = AutoTokenizer.from_pretrained(cfg.lm)
         else:
             tokenizer = AutoTokenizer.from_pretrained(cfg.lm)
             model = AutoModelForSequenceClassification.from_pretrained(cfg.lm, num_labels=num_labels, id2label = id2label, label2id = label2id)
